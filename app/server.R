8287979d1a14feb07369b9bea84d899e49b0ba8e
Varchasvi Vedula <52822460+varchasviv@users.noreply.github.com>
2022-02-20 18:49:46 -0500

    added neighborhood covid positivity rate indicator

--- app/server.R
+++ app/server.R
@@ -14,21 +14,47 @@ library(geojsonio)
 library(readr)
 library(leaflet)
 library("RSocrata")
+library(purrr)
 
 
 #========================================================================================================================================================================================
 
-restaurant <-read.csv(url("https://raw.githubusercontent.com/nychealth/coronavirus-data/master/latest/last7days-by-modzcta.csv"), 
-                      as.is = TRUE)
+restaurant <- read.csv("../data/restaurant.csv")
+restaurant$risk <- factor(restaurant$risk, levels = c("Very Low", "Low", "Moderate", "High"), ordered = T)
+# The restaurants dataset it big, so I have saved the cleaned version locally. To get the latest dataset, uncomment the chunk of code below
 
-df <- read.socrata(
-  "https://data.cityofnewyork.us/resource/43nn-pn8j.csv",
-  app_token = "g19Oad6Fq1r0KCmwHc0B14riH",
-  email     = "vvv2108@columbia.edu",
-  password  = "dymheh-1dozdo-jinGin"
-)
-df <- df[,c("dba","boro","latitude","longitude","zipcode","cuisine_description")]
-df <- unique(df)
+# restaurant <- read.socrata(
+#   "https://data.cityofnewyork.us/resource/43nn-pn8j.csv",
+#   app_token = "g19Oad6Fq1r0KCmwHc0B14riH",
+#   email     = "vvv2108@columbia.edu",
+#   password  = "dymheh-1dozdo-jinGin"
+# )
+# restaurant <- restaurant[,c("dba","boro","latitude","longitude","zipcode","cuisine_description")]
+# restaurant <- unique(restaurant)
+# 
+# covid <- read.csv(url("https://raw.githubusercontent.com/nychealth/coronavirus-data/master/latest/last7days-by-modzcta.csv"),
+#                   as.is = TRUE)
+# colnames(covid)[1] <- "zipcode"
+# restaurant <- merge(restaurant, covid, by = "zipcode")
+# 
+# risk <- function(percentpositivity_7day){
+#   if (percentpositivity_7day <= 1){
+#     return("Very Low")
+#   }
+#   else if (percentpositivity_7day > 1 & percentpositivity_7day <= 3){
+#     return("Low")
+#   }
+#   else if (percentpositivity_7day > 3 & percentpositivity_7day <= 5){
+#     return("Moderate")
+#   }
+#   else{
+#     return("High")
+#   }
+# }
+# restaurant$risk <- unlist(map(restaurant$percentpositivity_7day, risk))
+# restaurant$risk <- factor(restaurant$risk, levels = c("Very Low", "Low", "Moderate", "High"), ordered = T)
+# 
+# write.csv(restaurant,"../data/restaurant.csv", row.names = FALSE)
 
 #========================================================================================================================================================================================
 
@@ -37,13 +63,19 @@ shinyServer(function(input, output) {
     leaflet() %>%
       addTiles() %>%
       addProviderTiles("CartoDB.Voyager") %>%  
-      setView(lng = -73.935242, lat = 40.730610, zoom = 10) %>%
-      addMarkers(lng = -73.9618416, lat = 40.8081563,popup = "Here's Columbia!") 
+      setView(lng = -73.961035, lat = 40.744436, zoom = 12)
   })
-  reactive(leafletProxy("map", data = df %>% filter(zipcode == input$zip))%>%
+  palette <- c("chartreuse4", "palegreen", "lightpink1", "indianred4")
+  color <- colorFactor(palette =palette, restaurant$risk)
+  leafletProxy("map", data = restaurant)%>%
     clearShapes() %>%
-    addProviderTiles("CartoDB.Voyager") %>%    
-    addCircleMarkers(~longitude, ~latitude, radius=6,
-                     label = paste(df$dba, "-", df$zipcode))
-)
+    addProviderTiles("CartoDB.Voyager") %>%
+    addCircleMarkers(~longitude, ~latitude, radius=2,
+                     color = ~color(risk),
+                     label = paste(restaurant$dba, "-", restaurant$zipcode)) %>% 
+    addLegend("bottomright",
+              pal = color,
+              values = restaurant$risk,
+              title = "Covid Positive Rate",
+              opacity = 0.85) 
 })
--- app/ui.R
+++ app/ui.R
@@ -15,6 +15,7 @@ library(readr)
 library(leaflet)
 library(shinythemes)
 library(shinyWidgets)
+library(purrr)
 
 backgroundpic <- "https://images.unsplash.com/photo-1609945648638-cefddce6e6d8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2532&q=80" 
 
@@ -24,29 +25,16 @@ shinyUI(
     navbarPage(theme = shinytheme("sandstone"), collapsible = TRUE,
                title= "",
                id="nav",
-               windowTitle = "The NYC Department of Homeless Services during the COVID-19 Pandemic",
+               windowTitle = "NYC Restaurants",
                header = tagList(
                  useShinydashboard()
                ),
 
                
                tabPanel('Restaurants by Covid Area', icon = icon("viruses"),
-                        titlePanel("Restaurant Location"),
+                        titlePanel("Restaurant Location and Neighborhood Covid Positivity Rate"),
                         
-                        leafletOutput("map", width="100%", height=700),
-                        
-                        absolutePanel(id = "choices", class = "panel panel-default",
-                                      top = 160, left = 40, width = 240, fixed=FALSE,
-                                      draggable = TRUE, height = 470,
-                                      tags$h1("Where Do You Want To Eat?",
-                                              align = "left", style = "font-size:20px"),
-                                      numericInput(
-                                        "zip",
-                                        "Zip Code",
-                                        value = 10027,
-                                        min = 10001,
-                                        max = 10229)
-                                      )
+                         leafletOutput("map", width="100%", height=700)
     )
   )
 )
